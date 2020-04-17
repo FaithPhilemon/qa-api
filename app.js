2c917694d69ad769ce65c1ee1ebb97e0c3cfb881
Faith Philemon <philemonfaith@gmail.com>
2020-04-17 01:40:29 -0700

    clean ups

--- app.js
+++ app.js
@@ -2,17 +2,30 @@
 
 const express = require('express');
 const app = express();
+const jsonParser = require("body-parser").json;
 
 
-app.use((req, res, next)=>{
-    console.log("First Piece of middleware");
+var jasonCheck = (req, res, next)=>{
+    if(req.body){
+        console.log("The sky is ", req.body.color);
+    }else{
+        console.log("There is no body property on this request");
+    }
     next();
-});
+}
 
-app.use("/about/:id", (req, res, next)=>{
-    console.log("Second Piece of middleware ID: ", req.params.id);
-    next();
-});
+
+app.use(jsonParser());
+
+// app.use((req, res, next)=>{
+//     console.log("First Piece of middleware");
+//     next();
+// });
+
+// app.use("/about/:id", (req, res, next)=>{
+//     console.log("Second Piece of middleware ID: ", req.params.id);
+//     next();
+// });
 
 var port = process.env.PORT || 3000;
 
